44feb6a8a9d20082f8ebb82becb998e4e05a9b44
Minseo Park <minsepar@c1r3s7.42seoul.kr>
2024-07-31 12:43:34 +0900

    .

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,11 @@
-import algoliasearch from 'https://cdn.jsdelivr.net/npm/algoliasearch@4.17.0/dist/algoliasearch-lite.esm.browser.js';
+async function loadAlgolia() {
+  const algoliasearch = await import(
+    "https://cdn.jsdelivr.net/npm/algoliasearch@4.17.0/dist/algoliasearch-lite.esm.browser.js"
+  );
+}
 
-fetch("http://ec2-35-77-196-143.ap-northeast-1.compute.amazonaws.com:3000/test")
+async function getAlgoliaData() {
+  fetch("http://ec2-35-77-196-143.ap-northeast-1.compute.amazonaws.com:3000/test")
   .then((data) => data.json())
   .then((records) => {
     const client = algoliasearch(
@@ -15,3 +20,4 @@ fetch("http://ec2-35-77-196-143.ap-northeast-1.compute.amazonaws.com:3000/test")
   .catch((error) => {
     console.error(error);
   });
+}
